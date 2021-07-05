8fa54dcf98be03f1ca08af948b24380bd1e8ffb4
Michael Carter <hickorytandem@gmail.com>
2021-07-06 08:30:52 +0900

    could not get delete action to work. Failed to finish. Sad.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# read all tasks
 get "tasks", to: "tasks#index"
 
-get "tasks/new", to: "tasks#new"
+get "tasks/new", to: "tasks#new" # display the form
 
+# read one task
 get "tasks/:id", to: "tasks#show", as: :task
 
+# create a task
+
 post "tasks", to: "tasks#create"
-
+# update a task
 get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-
+patch "tasks/:id", to: "tasks#update"
+# delete a task
+delete "tasks/:id", to: "tasks#destroy"
 end
+# but you won't need to write all of this.
+# 'resources :tasks' does the same as all of the above
 
